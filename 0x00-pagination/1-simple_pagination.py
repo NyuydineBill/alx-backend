2e7fe5b199b809872412859002c08ecf1d0152ee
NyuydineBill <billleynyuy@gmail.com>
2024-08-24 01:38:48 +0100

    DOne

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python3
 """
-Defines class Server that paginates a database of popular baby names
+This module provides a Server class to
+paginate a database of popular baby names.
 """
+
+import importlib.util
 import csv
-import math
-from typing import List, Tuple
+from typing import List
 
+# Dynamically import the module with a hyphen in the name
+module_name = '0-simple_pagination'
+module_spec = importlib.util.spec_from_file_location(module_name, './0-simple_pagination.py')
+module = importlib.util.module_from_spec(module_spec)
+module_spec.loader.exec_module(module)
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Takes 2 integer arguments and returns a tuple of size two
-    containing the start and end index corresponding to the range of
-    indexes to return in a list for those pagination parameters
-    Args:
-        page (int): page number to return (pages are 1-indexed)
-        page_size (int): number of items per page
-    Return:
-        tuple(start_index, end_index)
-    """
-    start, end = 0, 0
-    for i in range(page):
-        start = end
-        end += page_size
-
-    return (start, end)
-
+index_range = module.index_range
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -47,20 +37,21 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Takes 2 integer arguments and returns requested page from the dataset
-        Args:
-            page (int): required page number. must be a positive integer
-            page_size (int): number of records per page. must be a +ve integer
-        Return:
-            list of lists containing required data from the dataset
-        """
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
+        Retrieves a page of data from the dataset.
 
+        :param page: The current page number (1-indexed)
+        :param page_size: The number of items per page
+        :return: A list of rows corresponding to the specified page
+        """
+        assert isinstance(page, int) and page > 0, \
+            "Page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size must be an integer greater than 0"
+
+        start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
-        data_length = len(dataset)
-        try:
-            index = index_range(page, page_size)
-            return dataset[index[0]:index[1]]
-        except IndexError:
+
+        if start_index >= len(dataset):
             return []
+
+        return dataset[start_index:end_index]
